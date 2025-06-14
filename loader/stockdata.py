4b198bbc5444388cb5f230d58719cce2724ce269
Leo <withhope2011@hotmail.com>
2025-06-14 09:30:53 +0800

    1

--- loader/stockdata.py
+++ loader/stockdata.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 import logging
 import json
+import os
 
 logging.basicConfig(level=logging.INFO)
 
@@ -47,6 +48,26 @@ def load(ticker):
     combined.to_csv(DATA_PATH, index=False)
     logging.info(f"Saved im {DATA_PATH}")
 
+def read(ticker, time):
+    DATA_PATH = f"data/{ticker}.csv"
+    if not os.path.exists(DATA_PATH):
+        logging.info(f"Data for {ticker} not found, loading ...")
+        load(ticker)
+    df = pd.read_csv(DATA_PATH, index_col=0, parse_dates=True)
+    dt = pd.to_datetime(time, format="%Y-%m-%dT%H:%M:%S.000Z", utc=True)
+    df = df[~df.index.duplicated(keep="first")]
+    df = df.sort_index()
+    df = df[df.index > dt]
+    df.reset_index(inplace=True)
+    df.rename(columns={df.columns[0]: 'index'}, inplace=True)
+    df['index'] = pd.to_datetime(df['index'], utc=True).dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    desired_order = [
+        "Close", "Dividends", "High", "Interval", "Low", "Open", "Stock Splits", "Volume", "index"
+    ]
+    columns = [col for col in desired_order if col in df.columns]
+    df = df[columns]
+    return json.loads(df.to_json(orient='records'))
+
 def update(ticker, time):
     # The `time` is Javascript time.
     logging.info(f"Updating for {ticker} ...")
@@ -58,33 +79,42 @@ def update(ticker, time):
     if delta.total_seconds() < 60:
         logging.info("No update needed")
         return
-    
+
+    delta_minutes = int(delta.total_seconds() // 60)
+    delta_days = delta.days
+    intervals = []
+
     if delta < pd.Timedelta(days=7):
-        delta_str = f"{int(delta.total_seconds() // 60)}m"
-        intervals = [
-            ("1m", delta_str)
-        ]
+        if delta_minutes < 60:
+            delta_str = f"{delta_minutes}m"
+        elif delta_minutes < 1440:
+            delta_str = "1d"
+        else:
+            delta_str = f"{delta_minutes // 1440}d"
+        intervals = [("1m", delta_str)]
     elif delta < pd.Timedelta(days=60):
-        delta_str = f"{int(delta.total_seconds() // 60)}m"
+        delta_str = f"{delta_minutes // 1440}d"
         intervals = [
             ("1m", "7d"),
-            ("2m", delta_str)
-        ]   
+            ("2m", delta_str),
+        ]
     elif delta < pd.Timedelta(days=730):
-        delta_str = f"{int(delta.total_seconds() // 60)}m"
+        delta_str = f"{delta_minutes // 1440}d"
         intervals = [
             ("1m", "7d"),
             ("2m", "60d"),
             ("60m", delta_str)
         ]
     else:
-        delta_str = f"{int(delta.total_seconds() // 86400)}d"
+        # `d` haben keine Begrenzung
+        delta_days = int(delta.total_seconds() // 86400)
         intervals = [
             ("1m", "7d"),
             ("2m", "60d"),
             ("60m", "730d"),
-            ("1d", delta_str)
+            ("1d", f"{delta_days}d")
         ]
+
     Ticker = yf.Ticker(ticker)
     full_data = []
     for interval, period in intervals:
@@ -104,11 +134,10 @@ def update(ticker, time):
     combined = pd.concat(full_data)
     combined = combined[~combined.index.duplicated(keep="first")]
     combined = combined.sort_index()
+    combined = combined[combined.index > dt]
     combined.reset_index(inplace=True)
-    combined['index'] = pd.to_datetime(combined['index'], utc=True)
-    combined.set_index('index', inplace=True)
-    combined.reset_index(inplace=True)
-    combined['index'] = combined['index'].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    combined.rename(columns={combined.columns[0]: 'index'}, inplace=True)
+    combined['index'] = pd.to_datetime(combined['index'], utc=True).dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
     desired_order = [
         "Close", "Dividends", "High", "Interval", "Low", "Open", "Stock Splits", "Volume", "index"
     ]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
-app = Flask(__name__)
 import pandas as pd
 import os
 import loader.stockdata as dl
+
+app = Flask(__name__)
 
 @app.route('/api/stock/data/<ticker>', methods=['GET'])
 def read_stock_data(ticker):
@@ -16,17 +17,24 @@ def read_stock_data(ticker):
             dl.load(ticker)
         stock_df = pd.read_csv(filepath)
         stock_df['index'] = pd.to_datetime(stock_df['index'], utc=True)
-        stock_df.set_index('index', inplace=True)
-        stock_df.reset_index(inplace=True)
         stock_df['index'] = stock_df['index'].dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
         return jsonify(stock_df.to_dict(orient='records'))
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 @app.route('/api/stock/data/<ticker>/<time>', methods=['GET'])
 def real_time_stock_data(ticker, time):
     json_data = dl.update(ticker, time)
     if json_data is None:
         return jsonify({"error": "Stock data not found"}), 404
     return jsonify(json_data)
+
+@app.route('/api/stock/data/update/<ticker>/<time>', methods=['GET'])
+def update_stock_data(ticker, time):
+    json_data = dl.read(ticker, time)
+    if json_data is None:
+        return jsonify({"error": "Stock data not found"}), 404
+    return jsonify(json_data)
+
 if __name__ == "__main__":
     app.run(debug=True)
